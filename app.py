facc9ad0e70ad992147734a97dfb2d2d3f6f0a7f
drrrrKim <150220406+drrrrKim@users.noreply.github.com>
2023-11-09 11:26:12 +0900

    feat : health func
    fastapi setting
    
    feat issue #1

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('first commit')
+from routes import health
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+
+app.include_router(health.router)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
