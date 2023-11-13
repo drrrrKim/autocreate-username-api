db29e4077542f77e94500c97a8001e5a7fd39f0e
drrrrKim <150220406+drrrrKim@users.noreply.github.com>
2023-11-13 11:22:10 +0900

    refactor : naming
    - delete async

--- routes/username.py
+++ routes/username.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
-from service.generate_data import create_name, create_adjectives
+from service.generate_data import create_username
 
 router = APIRouter()
 
-@router.get('/create')
-async def create_user_name():
-    first_adjectives = await create_adjectives()
-    second_name = await create_name()
-    username = first_adjectives + " " + second_name
-    return {"username":username}
+@router.post('/create')
+def create_user_name():
+    username = create_username()
+    if username:
+        return {"username":username}
+    else:
+        return {"status":"Unable to create user name"}
