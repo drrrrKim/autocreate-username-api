fa7e48cd31e9e94fc7e8b073e32108d3e7748809
drrrrKim <150220406+drrrrKim@users.noreply.github.com>
2023-11-13 11:33:05 +0900

    Modfiy : status code
    - post create sucess 201
    - username create fail 500

--- routes/username.py
+++ routes/username.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
 from service.generate_data import create_username
 
 router = APIRouter()
@@ -7,6 +8,12 @@ router = APIRouter()
 def create_user_name():
     username = create_username()
     if username:
-        return {"username":username}
+        return JSONResponse(
+            status_code=201,
+            content={"username": username}
+        )
     else:
-        return {"status":"Unable to create user name"}
+        raise HTTPException(
+            status_code=500,
+            detail="Unable to create user name"
+        )
