679649a4160a5a9c1c5b22b0df6b8e59f75c4d4d
drrrrKim <150220406+drrrrKim@users.noreply.github.com>
2023-11-13 11:16:43 +0900

    connect DB
    - simple test DB
    - select, insert query
    
    #6

--- service/generate_data.py
+++ service/generate_data.py
@@ -1,7 +1,8 @@
 import csv
 import random
 
-async def create_name():
+from db.connect import connect_db
+def create_name():
     with open('data/name_data.csv', 'r') as file:
         reader = csv.reader(file)
         data = list(reader)
@@ -9,10 +10,35 @@ async def create_name():
     random_row = random.choice(data)
     return random_row[0]
 
-async def create_adjectives():
+def create_adjectives():
     with open('data/adjectives.csv', 'r') as file:
         reader = csv.reader(file)
         data = list(reader)
 
     random_row = random.choice(data)
     return random_row[0]
+
+def create_username():
+    while(True):
+        first_adjectives = create_adjectives()
+        second_name = create_name()
+        username = first_adjectives + " " + second_name
+        username = "testname"
+        if overlap_username_chk(username):
+           continue
+        else:
+            return insert_username(username)
+        
+def overlap_username_chk(username):
+    conn = connect_db()
+    cursor = conn.cursor()
+    query = cursor.execute("SELECT username FROM users WHERE username = ?", (username,))
+    result = query.fetchone()
+    return result
+
+def insert_username(username):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (username) VALUES(?)", (username,))
+    conn.commit()
+    return username
